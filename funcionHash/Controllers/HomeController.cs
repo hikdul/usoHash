4dc440a9e0af27d22ebaf3c39a9cbbc2d279220f
Hector Contreras <hikdul.eva@gmail.com>
2020-10-05 18:43:59 -0500

    hash tipo SHA256

---
+++ funcionHash/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using funcionHash.Models;
+using funcionHash.Helpers;
+
+namespace funcionHash.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string pruebaHash(string valor)
+        {
+            string valorReturn = funcionHA.cifrarDatos(valor);
+            return valorReturn;
+        }
+
+        public bool comparar(string valorCifrado, string valornuevo)
+        {
+            if (funcionHA.cifrarDatos(valornuevo) != valorCifrado)
+                return false;
+            return true;
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ funcionHash/Helpers/funcionHA.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funcionHash.Helpers
+{
+    public static class funcionHA
+    {
+
+		public static string cifrarDatos(string data)
+		{
+			SHA256Managed sha = new SHA256Managed();
+			byte[] dataSinCifrar = Encoding.Default.GetBytes(data);
+			byte[] dataCifrada = sha.ComputeHash(dataSinCifrar);
+
+			return BitConverter.ToString(dataCifrada).Replace("-", "");
+		}
+
+
+
+	}
+}
